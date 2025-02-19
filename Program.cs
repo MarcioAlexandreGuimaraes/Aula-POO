ba3202af30d77b64a56e105d1ca1c55d66f867a8
MarcioAlexandreGuimaraes <marcio.guimaraes2615@gmail.com>
2025-02-18 21:10:52 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿double A, B, C, R, S, D;
+Console.WriteLine("Digite o valor de A");
+A = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite o valor de B");
+B = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite o valor de C");
+C = double.Parse(Console.ReadLine());
+
+R = Math.Pow(A + B, 2);
+
+S = Math.Pow(B + C, 2);
+
+D = (R + S) / 2;
+
+Console.Write(D);
