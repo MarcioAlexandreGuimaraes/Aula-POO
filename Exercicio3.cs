845222dbdf42d2425911ae5e26628c87b383b58d
MarcioAlexandreGuimaraes <marcio.guimaraes2615@gmail.com>
2025-02-18 21:34:56 -0300

    Add files via upload

---
+++ Exercicio3.cs
@@ -0,0 +1,13 @@
+﻿int notaum, notadois, notatres, resultado;
+Console.WriteLine("Digite a primeira nota:");
+notaum= int.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite a segunda nota:");
+notadois= int.Parse(Console.ReadLine());;
+
+Console.WriteLine("Digite a terceira nota:");
+notatres = int.Parse(Console.ReadLine());
+
+resultado = (notaum * 2 + notadois * 3 + notatres * 5) / 2;
+
+Console.Write(resultado);
